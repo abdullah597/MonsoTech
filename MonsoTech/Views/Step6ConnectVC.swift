c3e43931fa9c485fe009ee7dfa4b16c4f1ec3cf9
Haroon Munir <65074594+ehtisham-badar@users.noreply.github.com>
2024-06-14 22:43:46 +0500

    final updates

--- MonsoTech/Views/Step6ConnectVC.swift
+++ MonsoTech/Views/Step6ConnectVC.swift
@@ -11,19 +11,12 @@ class Step6ConnectVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextStep(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: HomeVC.self)) as? HomeVC {
+            Utilities.shared.pushViewController(currentViewController: self, toViewController: secondViewController, animated: true)
+        }
     }
-    */
-
 }
