83edb5ba36430b11345b65ec7e09e492e0fd5eb9
Haroon Munir <65074594+ehtisham-badar@users.noreply.github.com>
2024-06-23 16:17:29 +0500

    updates

--- MonsoTech/Views/Auth/SignupVC.swift
+++ MonsoTech/Views/Auth/SignupVC.swift
@@ -15,13 +15,24 @@ class SignupVC: UIViewController {
     @IBOutlet weak var lblError: UILabel!
     @IBOutlet weak var loader: UIActivityIndicatorView!
     
+    @IBOutlet weak var atleastLcUcICon: UIImageView!
+    @IBOutlet weak var atleast1Icon: UIImageView!
     @IBOutlet weak var termsLabel: UILabel!
     @IBOutlet weak var errorView: UIView!
+    @IBOutlet weak var min8Icon: UIImageView!
+    @IBOutlet weak var agreeIcon: UIImageView!
+    @IBOutlet weak var hideShowBtn: UIButton!
+    
+    @IBOutlet weak var signupBtn: UIButton!
     var applicationContext: MSALPublicClientApplication?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loader.isHidden = true
+        passwordTF.delegate = self
+        updateIcons(for: "")
+        signupBtn.isEnabled = false
+        setupTermsLabel()
     }
     @IBAction func back(_ sender: Any) {
         Utilities.shared.popViewController(currentViewController: self, animated: true)
@@ -39,6 +50,11 @@ class SignupVC: UIViewController {
         signUp(email: email, password: password)
     }
     
+    @IBAction func hideShowPassword(_ sender: Any) {
+        passwordTF.isSecureTextEntry.toggle()
+        let imageName = passwordTF.isSecureTextEntry ? "showPassIcon" : "hidePassIcon"
+        hideShowBtn.setImage(UIImage(named: imageName), for: .normal)
+    }
     @IBAction func login(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: LoginVC.self)) as? LoginVC {
@@ -63,5 +79,130 @@ class SignupVC: UIViewController {
         if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: ConnectDeviceVC.self)) as? ConnectDeviceVC {
             Utilities.shared.pushViewController(currentViewController: self, toViewController: secondViewController, animated: true)
         }
+    }
+}
+
+extension SignupVC: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        
+        let newText = (text as NSString).replacingCharacters(in: range, with: string)
+        updateIcons(for: newText)
+        
+        return true
+    }
+    
+    func updateIcons(for password: String) {
+        let min8Characters = password.count >= 8
+        let containsNumber = password.rangeOfCharacter(from: .decimalDigits) != nil
+        let containsLetter = password.rangeOfCharacter(from: .letters) != nil
+        
+        min8Icon.image = UIImage(named: min8Characters ? "tick" : "cross")
+        atleast1Icon.image = UIImage(named: containsNumber ? "tick" : "cross")
+        atleastLcUcICon.image = UIImage(named: containsLetter ? "tick" : "cross")
+        
+        let isPasswordValid = min8Characters && containsNumber && containsLetter
+        signupBtn.isEnabled = isPasswordValid
+    }
+}
+
+extension SignupVC {
+    func setupTermsLabel() {
+        let fullText = "I agree to the company Term of Service and Privacy Policy"
+        let termsText = "Term of Service"
+        let privacyText = "Privacy Policy"
+        
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let fullRange = NSRange(location: 0, length: fullText.count)
+        let termsRange = (fullText as NSString).range(of: termsText)
+        let privacyRange = (fullText as NSString).range(of: privacyText)
+        
+        // Set default color for the whole text
+        attributedString.addAttribute(.foregroundColor, value: UIColor.gray, range: fullRange)
+        
+        // Set color for "Term of Service" and "Privacy Policy"
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: termsRange)
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: privacyRange)
+        
+        // Set attributed text to the label
+        termsLabel.attributedText = attributedString
+        termsLabel.isUserInteractionEnabled = true
+        
+        // Add gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOnLabel(_:)))
+        termsLabel.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTapOnLabel(_ gesture: UITapGestureRecognizer) {
+        guard let text = termsLabel.attributedText?.string else { return }
+        
+        let termsText = "Term of Service"
+        let privacyText = "Privacy Policy"
+        
+        let termsRange = (text as NSString).range(of: termsText)
+        let privacyRange = (text as NSString).range(of: privacyText)
+        
+        if gesture.didTapAttributedTextInLabel(label: termsLabel, inRange: termsRange) {
+            print("Term of Service tapped")
+            openWebView(urlString: "www.monso.tech/termsofservice_EN.pdf", isTerms: true)
+        } else if gesture.didTapAttributedTextInLabel(label: termsLabel, inRange: privacyRange) {
+            print("Privacy Policy tapped")
+            openWebView(urlString: "www.monso.tech/privacypolicy_EN.pdf", isTerms: false)
+        } else {
+            print("unidentified")
+        }
+    }
+    
+    func openWebView(urlString: String, isTerms: Bool) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: WebViewController.self)) as? WebViewController {
+            secondViewController.urlString = urlString
+            secondViewController.isTerms = isTerms
+            Utilities.shared.pushViewController(currentViewController: self, toViewController: secondViewController, animated: true)
+        }
+    }
+}
+
+extension UITapGestureRecognizer {
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        guard let attributedText = label.attributedText else { return false }
+        
+        let textStorage = NSTextStorage(attributedString: attributedText)
+        let layoutManager = NSLayoutManager()
+        textStorage.addLayoutManager(layoutManager)
+        
+        let textContainer = NSTextContainer(size: CGSize(width: label.bounds.width, height: CGFloat.greatestFiniteMagnitude))
+        textContainer.lineFragmentPadding = 0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        layoutManager.addTextContainer(textContainer)
+        
+        layoutManager.glyphRange(for: textContainer) // Ensures proper layout
+        
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(
+            x: (label.bounds.size.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+            y: (label.bounds.size.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y
+        )
+        
+        let locationOfTouchInTextContainer = CGPoint(
+            x: locationOfTouchInLabel.x - textContainerOffset.x,
+            y: locationOfTouchInLabel.y - textContainerOffset.y
+        )
+        
+        let characterIndex = layoutManager.characterIndex(
+            for: locationOfTouchInTextContainer,
+            in: textContainer,
+            fractionOfDistanceBetweenInsertionPoints: nil
+        )
+        
+        print("locationOfTouchInLabel: \(locationOfTouchInLabel)")
+        print("textBoundingBox: \(textBoundingBox)")
+        print("textContainerOffset: \(textContainerOffset)")
+        print("locationOfTouchInTextContainer: \(locationOfTouchInTextContainer)")
+        print("characterIndex: \(characterIndex)")
+        
+        return NSLocationInRange(characterIndex, targetRange)
     }
 }
---
+++ MonsoTech/Views/Auth/WebViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WebViewController.swift
+//  MonsoTech
+//
+//  Created by Ehtisham Badar on 23/06/2024.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    var urlString: String?
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    @IBOutlet weak var mainView: UIView!
+    var isTerms: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if isTerms {
+            lblTitle.text = "Term of Service"
+        } else {
+            lblTitle.text = "Privacy Policy"
+        }
+        Utilities.shared.setTopCorners(view: mainView, radius: 30)
+        Utilities.shared.setTopCorners(view: webView, radius: 30)
+
+        if let urlString = urlString, let url = URL(string: urlString) {
+            webView.load(URLRequest(url: url))
+        }
+    }
+    @IBAction func back(_ sender: Any) {
+        Utilities.shared.popViewController(currentViewController: self, animated: true)
+    }
+}
