1a620ae37e39aaf705d763bbd64b65522f710f1e
Ehtisham Badar <ehtisham.badar@curemd.com>
2024-06-25 12:21:13 +0500

    updates in 9,14, 21

--- MonsoTech/Views/Connect Slave Device/ConnectSlaveDeviceVC.swift
+++ MonsoTech/Views/Connect Slave Device/ConnectSlaveDeviceVC.swift
@@ -19,8 +19,8 @@ class ConnectSlaveDeviceVC: UIViewController, UITextFieldDelegate {
         Utilities.shared.popViewController(currentViewController: self, animated: true)
     }
     @IBAction func connect(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: ConnectFailedVC.self)) as? ConnectFailedVC {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        if let secondViewController = storyboard.instantiateViewController(withIdentifier: String(describing: HomeVC.self)) as? HomeVC {
             Utilities.shared.pushViewController(currentViewController: self, toViewController: secondViewController, animated: true)
         }
     }
